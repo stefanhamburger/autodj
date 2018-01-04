0aa8530328d9420568c2281a8cb20c867e6b3ef2
stefanhamburger <stefan.hamburger@rwth-aachen.de>
2018-01-04 18:11:30 +0100

    Cap bpm at 180 (default was 200)

--- server/worker/tempoRecognition.mjs
+++ server/worker/tempoRecognition.mjs
@@ -1,6 +1,10 @@
 //import MusicTempo from 'music-tempo';
 const MusicTempo = require('music-tempo');//tiny-worker does not yet support ES modules?
 
+//The bpm range that should be detected (60-180 bpm)
+const MIN_BPM = 60;
+const MAX_BPM = 180;
+
 let waveform;
 let hasReceivedLength = false;
 let pos = 0;
@@ -16,10 +20,16 @@ onmessage = function messageHandler(ev) {
 
     //if we are done
     if (pos === waveform.length) {
-      //console.log('Received audio buffer! Calculating tempo...');
-      const mt = new MusicTempo(waveform);
+      //start tempo recognition
+      const mt = new MusicTempo(waveform, {
+        maxBeatInterval: 60 / MIN_BPM,
+        minBeatInterval: 60 / MAX_BPM,
+      });
+
       //MusicTempo expects a PCM with a 44,100 sample rate. So we need to convert the tempo since we have a 48,000 sample rate
-      const bpm = mt.tempo * 48000 / 44100;
+      const bpm = Math.round(mt.tempo * 48000 / 44100 * 1000) / 1000;
+
+      //send recognized tempo to main thread
       postMessage(bpm);//TODO: need to send more data than just tempo
     }
   }

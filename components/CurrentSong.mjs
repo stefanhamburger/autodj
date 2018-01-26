4f8c060a3046b830310a5cb14b8fd214677ba7d1
stefanhamburger <stefan.hamburger@rwth-aachen.de>
2018-01-26 17:54:17 +0100

    Refactor tempo display in UI

--- components/CurrentSong.mjs
+++ components/CurrentSong.mjs
@@ -3,14 +3,12 @@ import React from 'react';
 function CurrentSong({ name, bpm }) {
   //Generate JSX from bpm value
   let jsxTempo;
-  if (bpm !== undefined) {
-    if (bpm > 0) {
-      jsxTempo = <React.Fragment>, {bpm} bpm</React.Fragment>;
-    } else {
-      jsxTempo = ' [Tempo detection failed]';
-    }
-  } else {
+  if (bpm === undefined) {
     jsxTempo = ' [Detecting tempo...]';
+  } else if (bpm === 0) {
+    jsxTempo = ' [Tempo detection failed]';
+  } else {
+    jsxTempo = <React.Fragment> [{bpm} bpm]</React.Fragment>;
   }
 
   return (

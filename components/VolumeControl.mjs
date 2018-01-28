b9cdd7a406a6fdd838640b1bb442c54408b3a712
stefanhamburger <stefan.hamburger@rwth-aachen.de>
2018-01-28 02:33:41 +0100

    Add CSS to VolumeControl

--- components/VolumeControl.mjs
+++ components/VolumeControl.mjs
@@ -1,11 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
+import getViridisColor from '../client/view/viridis.mjs';
+
+const Container = styled.label`
+  &:hover > input {
+    display: block;
+  }
+`;
+
+const Button = styled.span`
+  display: inline-block;
+  width: 24px;
+  height: 24px;
+  border: 1px solid ${getViridisColor(0)};
+  border-radius: 5px;
+  text-align: center;
+`;
+
+const Slider = styled.input`
+  display: none;
+  transform: rotate(-90deg);
+  width: 100px;
+  height: 15px;
+  position: absolute;
+  margin-left: 74px;
+  margin-top: 40px;
+  z-index: 3;
+  background-color: rgba(240, 240, 240, 0.8);
+`;
 
 function VolumeControl({ volumeChangeCallback }) {
   return (
-    <label>
-      <span style={{ marginRight: '5px' }}>Volume:</span>
-      <input type="range" min="0" defaultValue="10" max="100" step="1" onInput={event => volumeChangeCallback(Number(event.target.value) / 100)} />
-    </label>
+    <Container>
+      <Button>🔉</Button>
+      <Slider type="range" min="0" defaultValue="10" max="100" step="1" onInput={event => volumeChangeCallback(Number(event.target.value) / 100)} />
+    </Container>
   );
 }
 

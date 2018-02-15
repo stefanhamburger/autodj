c321a5ea769cead23af5c2ae7d930b407442ecb4
stefanhamburger <stefan.hamburger@rwth-aachen.de>
2018-02-15 15:05:21 +0100

    📚 Add link to consoleColors comment

--- server/consoleColors.mjs
+++ server/consoleColors.mjs
@@ -1,4 +1,7 @@
-//Output colors in the Node.js console to improve debugging
+/**
+ * Output colors in the Node.js console to improve debugging
+ * For more colors, see https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+ */
 
-export const magenta = text => `\x1b[0;35m${text}\x1b[0m`;
 export const green = text => `\x1b[0;32m${text}\x1b[0m`;
+export const magenta = text => `\x1b[0;35m${text}\x1b[0m`;

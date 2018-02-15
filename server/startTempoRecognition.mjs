e4a06e0bab682fbf08caebe1bcfecc6466da1974
stefanhamburger <stefan.hamburger@rwth-aachen.de>
2018-02-15 14:45:20 +0100

    ♻️ Promisify tempo recognition to later allow await & try/catch in audioBuffer

--- server/startTempoRecognition.mjs
+++ server/startTempoRecognition.mjs
@@ -12,28 +12,30 @@ const SONG_END_LENGTH = 60 * SAMPLE_RATE;
 
 
 /** Creates a worker to detect the tempo of the given waveform data, and calls the callback function with the bpm */
-function createWorker(waveformArray, callback) {
-  const worker = new Worker('server/worker/tempoRecognition.mjs');
+function createWorker(waveformArray) {
+  return new Promise((resolve) => {
+    const worker = new Worker('server/worker/tempoRecognition.mjs');
 
-  //add listener to result
-  worker.onmessage = (msg) => {
-    callback(msg.data);
-    worker.terminate();
-  };
+    //add listener to result
+    worker.onmessage = (msg) => {
+      resolve(msg.data);
+      worker.terminate();
+    };
 
-  //send number of samples
-  worker.postMessage(waveformArray.length);
+    //send number of samples
+    worker.postMessage(waveformArray.length);
 
-  //to prevent Node from blocking and using too much RAM, send waveform in blocks of 48k samples (1 second)
-  let pos = 0;
-  const sendNextSamples = () => {
-    worker.postMessage(Array.from(waveformArray.slice(pos, pos + 1 * SAMPLE_RATE)));
-    pos += 1 * SAMPLE_RATE;
-    if (pos < waveformArray.length) {
-      setTimeout(sendNextSamples);
-    }
-  };
-  sendNextSamples();
+    //to prevent Node from blocking and using too much RAM, send waveform in blocks of 48k samples (1 second)
+    let pos = 0;
+    const sendNextSamples = () => {
+      worker.postMessage(Array.from(waveformArray.slice(pos, pos + 1 * SAMPLE_RATE)));
+      pos += 1 * SAMPLE_RATE;
+      if (pos < waveformArray.length) {
+        setTimeout(sendNextSamples);
+      }
+    };
+    sendNextSamples();
+  });
 }
 
 
@@ -48,7 +50,7 @@ export default async function startTempoRecognition(session, song, isFirstSong =
 
   //if song is too short, we detect tempo across the whole song
   if (waveformArray.length < SONG_MIN_LENGTH) {
-    createWorker(waveformArray, (bpm) => {
+    createWorker(waveformArray).then((bpm) => {
       console.log(`[${session.sid}] Song ${song.songRef.name} starts and ends with ${bpm} bpm`);
       session.emitEvent({ type: 'TEMPO_INFO_START', id: song.id, bpm });
       session.emitEvent({ type: 'TEMPO_INFO_END', id: song.id, bpm });
@@ -57,7 +59,7 @@ export default async function startTempoRecognition(session, song, isFirstSong =
     //if this is the first song we are playing, tempo at beginning doesn't matter
     if (!isFirstSong) {
       //tempo at beginning of song
-      createWorker(waveformArray.slice(0, SONG_START_LENGTH), (bpm) => {
+      createWorker(waveformArray.slice(0, SONG_START_LENGTH)).then((bpm) => {
         console.log(`[${session.sid}] Song ${song.songRef.name} starts with ${bpm} bpm`);
         session.emitEvent({ type: 'TEMPO_INFO_START', id: song.id, bpm });
       });
@@ -67,7 +69,7 @@ export default async function startTempoRecognition(session, song, isFirstSong =
     }
 
     //tempo at end of song
-    createWorker(waveformArray.slice(waveformArray.length - SONG_END_LENGTH), (bpm) => {
+    createWorker(waveformArray.slice(waveformArray.length - SONG_END_LENGTH)).then((bpm) => {
       console.log(`[${session.sid}] Song ${song.songRef.name} ends with ${bpm} bpm`);
       session.emitEvent({ type: 'TEMPO_INFO_END', id: song.id, bpm });
     });

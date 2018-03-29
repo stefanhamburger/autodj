11facc0c701e66c3fee46658b7ea5ba3fc652925
stefanhamburger <9825318+stefanhamburger@users.noreply.github.com>
2018-03-29 23:53:04 +0200

    ♻️ Refactor promise storage

--- server/lib/promiseStorage.mjs
+++ server/lib/promiseStorage.mjs
@@ -1,8 +1,13 @@
+/**
+ * A library to create promises and store the resolve/reject callbacks, so that they can later be called.
+ * This allows us to use promises for interprocess communication.
+ * TODO: This library can be optimized by removing promises once they are resolved/rejected.
+ */
 export default function PromiseStorage() {
-  const promiseStorage = {};
+  const storage = {};
   return {
-    create: name => new Promise((resolve, reject) => { promiseStorage[name] = { resolve, reject }; }),
-    resolve: (name, ...args) => promiseStorage[name].resolve(...args),
-    reject: (name, ...args) => promiseStorage[name].reject(...args),
+    create: name => new Promise((resolve, reject) => { storage[name] = { resolve, reject }; }),
+    resolve: (name, ...args) => storage[name].resolve(...args),
+    reject: (name, ...args) => storage[name].reject(...args),
   };
 }

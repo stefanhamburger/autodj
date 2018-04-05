9071c20206dfc6451e72549b843c209c3ca7d0dc
stefanhamburger <9825318+stefanhamburger@users.noreply.github.com>
2018-04-05 20:52:33 +0200

    🐛 Fix shared calculateDuration() function

--- shared/calculateDuration.mjs
+++ shared/calculateDuration.mjs
@@ -3,6 +3,16 @@
  * This must produce identical results on client- and server-side.
  */
 export default (duration, tempoAdjustment) => {
-  //TODO: need to calculate more precisely based on tempo-change.mjs
-  return Math.floor(duration / tempoAdjustment + 1);
+  let out = 0;
+  let samplesLeft = duration;
+
+  //process duration in blocks of 4,096 samples
+  //TODO: tempo adjustment can vary over time
+  while (samplesLeft > 0) {
+    const blockSize = Math.min(4096, samplesLeft);
+    out += Math.ceil(blockSize / tempoAdjustment);
+    samplesLeft -= blockSize;
+  }
+
+  return out;
 };

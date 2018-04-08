d686354de8951cbd43d4a14a17e8fd80fd00f886
stefanhamburger <9825318+stefanhamburger@users.noreply.github.com>
2018-04-08 17:11:58 +0200

    🐛 Fix IPC cache

--- server/launchProcessInputHandler.mjs
+++ server/launchProcessInputHandler.mjs
@@ -1,46 +1,50 @@
-let cache = [];
+let fullBuffer;
 let totalLength = 0;
 let header = {};
 
 export default function inputHandler(callback, buffer) {
-  const arrayBuffer = buffer.buffer;
-
   if (totalLength === 0) {
-    const dv = new DataView(arrayBuffer);
     //parse header (type + length)
-    header.type = dv.getUint8(0);
-    header.id = dv.getUint32(1, true);
-    header.length = dv.getUint32(5, true);
+    header.type = buffer.readUInt8(0);
+    header.id = buffer.readUInt32LE(1);
+    header.length = buffer.readUInt32LE(5);
+
+    //set up buffer to store full message, and add body from received message
+    fullBuffer = new Uint8Array(header.length);
+    for (let i = 9; i < buffer.length; i += 1) {
+      fullBuffer[i - 9] = buffer.readUInt8(i);
+    }
+    totalLength += buffer.length - 9;
+  } else {
+    //add complete received buffer
+    fullBuffer.set(buffer, totalLength);
+    totalLength += buffer.length;
   }
 
-  totalLength += arrayBuffer.byteLength;
-  cache.push(arrayBuffer);
 
   if (totalLength < header.length) { //did not yet receive full message
     //do nothing, wait for more messages
   } else if (totalLength > header.length) { //message too long
     throw new Error(`Message length check failed, expected message to be ${header.length} but was at least ${totalLength}`);
   } else { //full message received
-    const fullBuffer = Buffer.concat(cache, totalLength);
-    const bufferBody = fullBuffer.buffer.slice(9);
-
     if (header.type === 0) { //string
       //convert ArrayBuffer to string
-      const text = String.fromCharCode.apply(null, new Uint8Array(bufferBody));
+      const text = String.fromCharCode.apply(null, new Uint8Array(fullBuffer));
       //parse JSON
       try {
         const obj = JSON.parse(text);
         callback(true, header.id, obj);
       } catch (err) {
-        throw new Error('Could not read JSON message from child');
+        throw new Error(`Could not read JSON message from child: ${err}`);
       }
     } else if (header.type === 1) { //binary
-      callback(false, header.id, bufferBody);
+      callback(false, header.id, fullBuffer.buffer);
     } else {
       throw new Error(`Unknown message type ${header.type} from child`);
     }
 
-    cache = [];
+    //reset variables
+    fullBuffer = undefined;
     totalLength = 0;
     header = {};
   }

366b0f7ea3b4822862678497fd7f3b7c8ef1d13c
stefanhamburger <9825318+stefanhamburger@users.noreply.github.com>
2018-04-08 16:05:27 +0200

    🐛 Implement IPC cache to get around 64k pipe buffer limitation in Linux

--- server/launchProcessInputHandler.mjs
+++ server/launchProcessInputHandler.mjs
@@ -1,30 +1,47 @@
+let cache = [];
+let totalLength = 0;
+let header = {};
+
 export default function inputHandler(callback, buffer) {
   const arrayBuffer = buffer.buffer;
-  const dv = new DataView(arrayBuffer);
-  //parse header (type + length), perform length integrity check
-  const type = dv.getUint8(0);
-  const id = dv.getUint32(1, true);
-  const length = dv.getUint32(5, true);
-  if (arrayBuffer.byteLength !== 9 + length) {
-    //TODO: do not throw error but temporarily store buffer contents, and check length again upon next message, see Buffer.concat()
-    throw new Error(`Message length check failed, expected message to be ${9 + length} but was ${arrayBuffer.byteLength}`);
+
+  if (totalLength === 0) {
+    const dv = new DataView(arrayBuffer);
+    //parse header (type + length)
+    header.type = dv.getUint8(0);
+    header.id = dv.getUint32(1, true);
+    header.length = dv.getUint32(5, true);
   }
 
-  const bufferBody = arrayBuffer.slice(9);
+  totalLength += arrayBuffer.byteLength;
+  cache.push(arrayBuffer);
 
-  if (type === 0) { //string
-    //convert ArrayBuffer to string
-    const text = String.fromCharCode.apply(null, new Uint8Array(bufferBody));
-    //parse JSON
-    try {
-      const obj = JSON.parse(text);
-      callback(true, id, obj);
-    } catch (err) {
-      throw new Error('Could not read JSON message from child');
+  if (totalLength < header.length) { //did not yet receive full message
+    //do nothing, wait for more messages
+  } else if (totalLength > header.length) { //message too long
+    throw new Error(`Message length check failed, expected message to be ${header.length} but was at least ${totalLength}`);
+  } else { //full message received
+    const fullBuffer = Buffer.concat(cache, totalLength);
+    const bufferBody = fullBuffer.buffer.slice(9);
+
+    if (header.type === 0) { //string
+      //convert ArrayBuffer to string
+      const text = String.fromCharCode.apply(null, new Uint8Array(bufferBody));
+      //parse JSON
+      try {
+        const obj = JSON.parse(text);
+        callback(true, header.id, obj);
+      } catch (err) {
+        throw new Error('Could not read JSON message from child');
+      }
+    } else if (header.type === 1) { //binary
+      callback(false, header.id, bufferBody);
+    } else {
+      throw new Error(`Unknown message type ${header.type} from child`);
     }
-  } else if (type === 1) { //binary
-    callback(false, id, bufferBody);
-  } else {
-    throw new Error(`Unknown message type ${type} from child`);
+
+    cache = [];
+    totalLength = 0;
+    header = {};
   }
 }

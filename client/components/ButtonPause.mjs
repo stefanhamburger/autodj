626565f4a434663614b8b51030e75bacf7ab4cb5
stefanhamburger <9825318+stefanhamburger@users.noreply.github.com>
2018-04-25 15:39:25 +0200

    🐛 Allow pausing/unpausing audio via keyboard

--- client/components/ButtonPause.mjs
+++ client/components/ButtonPause.mjs
@@ -3,6 +3,6 @@ import React from 'react';
 export default function ButtonPause({ isPaused, pauseCallback }) {
   return (
     //alternatively, we could use ▶/⏸ but they have a box around them
-    <button onMouseDown={pauseCallback}>{ isPaused ? '►' : '❚❚'}</button>
+    <button onClick={pauseCallback}>{ isPaused ? '►' : '❚❚'}</button>
   );
 }

99d8ffc047f3c2fc6da51b92cb620b96a26ea724
stefanhamburger <9825318+stefanhamburger@users.noreply.github.com>
2018-04-26 14:45:32 +0200

    🗑️ Remove song.readyPromise
    We only add songs to the session once they're ready, so this is no longer needed.

--- server/playlist.mjs
+++ server/playlist.mjs
@@ -33,7 +33,7 @@ const generateId = (session) => {
  * If it fails, a different follow-up song is picked.
  * @param session
  */
-export function addFollowUpSong(session) {
+export async function addFollowUpSong(session) {
   //get random audio file
   const files = fileManager.getFiles(session.collection);
   const randomFile = files[Math.floor(Math.random() * files.length)];
@@ -50,74 +50,68 @@ export function addFollowUpSong(session) {
   });
 
   songWrapper.ready = false;
-  songWrapper.readyPromise = new Promise(async (resolve) => {
-    const previousSongs = session.currentSongs.filter(entry => entry.id !== songWrapper.id);
+  const previousSongs = session.currentSongs.filter(entry => entry.id !== songWrapper.id);
 
-    //wait until previous songs have finished processing
-    await Promise.all(previousSongs.map(entry => entry.readyPromise));
-
-    //find the song that is right before this one (= song with the highest starting time)
-    const previousSong = previousSongs.reduce((accumulator, curSong) => {
-      if (curSong.ready === true && curSong.startTime > accumulator.startTime) {
-        return curSong;
-      } else {
-        return accumulator;
-      }
-    }, { startTime: Number.NEGATIVE_INFINITY });
-
-    songWrapper.song = await songWrapper.song;
-    songWrapper.totalLength = await songWrapper.song.duration;
-
-    //do tempo recognition - and only use song if recognition was successful
-    try {
-      songWrapper.tempo = await songWrapper.song.tempo;
-    } catch (error) {
-      console.log(`${consoleColors.magenta(`[${session.sid}]`)} Tempo detection failed for ${consoleColors.green(songWrapper.songRef.name)}; skipping this song.`);
-
-      //add another song
-      addFollowUpSong(session);
-
-      //stop processing this song
-      return;
+  //find the song that is right before this one (= song with the highest starting time)
+  const previousSong = previousSongs.reduce((accumulator, curSong) => {
+    if (curSong.ready === true && curSong.startTime > accumulator.startTime) {
+      return curSong;
+    } else {
+      return accumulator;
     }
+  }, { startTime: Number.NEGATIVE_INFINITY });
 
-    //the time in samples at which to start adding this song to the stream
-    songWrapper.startTime = previousSong.endTime - 15 * 48000;
+  songWrapper.song = await songWrapper.song;
+  songWrapper.totalLength = await songWrapper.song.duration;
 
-    songWrapper.tempoAdjustment = previousSong.tempoAdjustment * previousSong.tempo.bpmEnd / songWrapper.tempo.bpmStart;
-    songWrapper.endTime = songWrapper.startTime + calculateDuration(songWrapper.totalLength, songWrapper.tempoAdjustment);
+  //do tempo recognition - and only use song if recognition was successful
+  try {
+    songWrapper.tempo = await songWrapper.song.tempo;
+  } catch (error) {
+    console.log(`${consoleColors.magenta(`[${session.sid}]`)} Tempo detection failed for ${consoleColors.green(songWrapper.songRef.name)}; skipping this song.`);
 
-    session.emitEvent({
-      type: 'SONG_START',
-      id: songWrapper.id,
-      songName: songWrapper.songRef.name,
-      time: songWrapper.startTime / 48000,
-    });
-    session.emitEvent({
-      type: 'SONG_DURATION',
-      id: songWrapper.id,
-      origDuration: songWrapper.totalLength,
-      tempoAdjustment: songWrapper.tempoAdjustment,
-    });
+    //add another song
+    addFollowUpSong(session);
 
-    session.emitEvent({
-      type: 'TEMPO_INFO',
-      id: songWrapper.id,
-      bpmStart: songWrapper.tempo.bpmStart,
-      bpmEnd: songWrapper.tempo.bpmEnd,
-      beats: songWrapper.tempo.beats,
-    });
+    //stop processing this song
+    return;
+  }
 
-    //Notify client that waveform data is ready
-    await songWrapper.song.thumbnail;
-    session.emitEvent({ type: 'THUMBNAIL_READY', id: songWrapper.id });
+  //the time in samples at which to start adding this song to the stream
+  songWrapper.startTime = previousSong.endTime - 15 * 48000;
 
-    session.currentSongs.push(songWrapper);
-    console.log(`${consoleColors.magenta(`[${session.sid}]`)} Adding to playlist: ${consoleColors.green(songWrapper.songRef.name)}.`);
+  songWrapper.tempoAdjustment = previousSong.tempoAdjustment * previousSong.tempo.bpmEnd / songWrapper.tempo.bpmStart;
+  songWrapper.endTime = songWrapper.startTime + calculateDuration(songWrapper.totalLength, songWrapper.tempoAdjustment);
 
-    resolve();
-    songWrapper.ready = true;
+  session.emitEvent({
+    type: 'SONG_START',
+    id: songWrapper.id,
+    songName: songWrapper.songRef.name,
+    time: songWrapper.startTime / 48000,
   });
+  session.emitEvent({
+    type: 'SONG_DURATION',
+    id: songWrapper.id,
+    origDuration: songWrapper.totalLength,
+    tempoAdjustment: songWrapper.tempoAdjustment,
+  });
+
+  session.emitEvent({
+    type: 'TEMPO_INFO',
+    id: songWrapper.id,
+    bpmStart: songWrapper.tempo.bpmStart,
+    bpmEnd: songWrapper.tempo.bpmEnd,
+    beats: songWrapper.tempo.beats,
+  });
+
+  //Notify client that waveform data is ready
+  await songWrapper.song.thumbnail;
+  session.emitEvent({ type: 'THUMBNAIL_READY', id: songWrapper.id });
+
+  session.currentSongs.push(songWrapper);
+  console.log(`${consoleColors.magenta(`[${session.sid}]`)} Adding to playlist: ${consoleColors.green(songWrapper.songRef.name)}.`);
+
+  songWrapper.ready = true;
 }
 
 
@@ -128,7 +122,7 @@ export function addFollowUpSong(session) {
  * @param session
  * @param offset
  */
-export function addFirstSong(session, offset = 0) {
+export async function addFirstSong(session, offset = 0) {
   //get random audio file
   const files = fileManager.getFiles(session.collection);
   const randomFile = files[Math.floor(Math.random() * files.length)];
@@ -151,11 +145,30 @@ export function addFirstSong(session, offset = 0) {
     time: songWrapper.startTime / 48000,
   });
   songWrapper.ready = false;//we need to store ready state separately since we can't get a promise's state natively
-  songWrapper.readyPromise = new Promise(async (resolve, reject) => {
-    songWrapper.song = await songWrapper.song;
-    songWrapper.totalLength = await songWrapper.song.duration;
-    //TODO: we should only send the duration if we are sure we are going to keep this song, or at least allow overriding it
-    songWrapper.endTime = songWrapper.startTime + songWrapper.totalLength;
+  songWrapper.song = await songWrapper.song;
+  songWrapper.totalLength = await songWrapper.song.duration;
+  //TODO: we should only send the duration if we are sure we are going to keep this song, or at least allow overriding it
+  songWrapper.endTime = songWrapper.startTime + songWrapper.totalLength;
+  session.emitEvent({
+    type: 'SONG_DURATION',
+    id: songWrapper.id,
+    origDuration: songWrapper.totalLength,
+    tempoAdjustment: songWrapper.tempoAdjustment,
+  });
+
+  songWrapper.ready = true;
+
+  try {
+    //do tempo recognition - for first song, we do not need to wait until it is done
+    songWrapper.tempo = await songWrapper.song.tempo;
+  } catch (error) {
+    console.log(`${consoleColors.magenta(`[${session.sid}]`)} Tempo detection failed for ${consoleColors.green(songWrapper.songRef.name)}; finding new first song.`);
+
+    //if tempo detection failed, end this song
+    songWrapper.endTime = Math.max(songWrapper.startTime, session.encoderPosition) + 5 * 48000;//allow 5 seconds to process next file
+    songWrapper.totalLength = songWrapper.endTime - songWrapper.startTime;
+
+    //emit event that duration has changed
     session.emitEvent({
       type: 'SONG_DURATION',
       id: songWrapper.id,
@@ -163,47 +176,25 @@ export function addFirstSong(session, offset = 0) {
       tempoAdjustment: songWrapper.tempoAdjustment,
     });
 
-    resolve();
-    songWrapper.ready = true;
+    //notify audio buffer that we pick a follow-up song ourselves and don't need audio buffer to do it
+    songWrapper.hadTempoFailure = true;
 
-    try {
-      //do tempo recognition - for first song, we do not need to wait until it is done
-      songWrapper.tempo = await songWrapper.song.tempo;
-    } catch (error) {
-      console.log(`${consoleColors.magenta(`[${session.sid}]`)} Tempo detection failed for ${consoleColors.green(songWrapper.songRef.name)}; finding new first song.`);
+    //select another song
+    addFirstSong(session, songWrapper.endTime);
+    reject();
+    return;
+  }
 
-      //if tempo detection failed, end this song
-      songWrapper.endTime = Math.max(songWrapper.startTime, session.encoderPosition) + 5 * 48000;//allow 5 seconds to process next file
-      songWrapper.totalLength = songWrapper.endTime - songWrapper.startTime;
-
-      //emit event that duration has changed
-      session.emitEvent({
-        type: 'SONG_DURATION',
-        id: songWrapper.id,
-        origDuration: songWrapper.totalLength,
-        tempoAdjustment: songWrapper.tempoAdjustment,
-      });
-
-      //notify audio buffer that we pick a follow-up song ourselves and don't need audio buffer to do it
-      songWrapper.hadTempoFailure = true;
-
-      //select another song
-      addFirstSong(session, songWrapper.endTime);
-      reject();
-      return;
-    }
-
-    session.emitEvent({
-      type: 'TEMPO_INFO',
-      id: songWrapper.id,
-      bpmEnd: songWrapper.tempo.bpmEnd,
-      beats: songWrapper.tempo.beats,
-    });
-
-    //Notify client that waveform data is ready
-    await songWrapper.song.thumbnail;
-    session.emitEvent({ type: 'THUMBNAIL_READY', id: songWrapper.id });
-
-    setTimeout(addFollowUpSong.bind(null, session), 15000);
+  session.emitEvent({
+    type: 'TEMPO_INFO',
+    id: songWrapper.id,
+    bpmEnd: songWrapper.tempo.bpmEnd,
+    beats: songWrapper.tempo.beats,
   });
+
+  //Notify client that waveform data is ready
+  await songWrapper.song.thumbnail;
+  session.emitEvent({ type: 'THUMBNAIL_READY', id: songWrapper.id });
+
+  setTimeout(addFollowUpSong.bind(null, session), 15000);
 }
